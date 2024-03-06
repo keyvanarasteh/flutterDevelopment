1fff8a580755ff23ab629b74456b0d9106dd1fd5
keyvanarasteh <keyvan.arasteh@live.com>
2024-03-06 11:11:57 +0300

    stable code

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => ClientCubit(
-            darkMode: false,
-            language: "en",
-            rtl: false,
-          ),
-        ),
-      ],
-      child: BlocBuilder<ClientCubit, ClientState>(builder: (context, state) {
-        return MaterialApp.router(
-          themeMode: state.darkMode ? ThemeMode.dark : ThemeMode.light,
-          debugShowCheckedModeBanner: false,
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          routerConfig: routes,
-        );
-      }),
-=======
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: routes,
->>>>>>> parent of acd7694 (read system language and themeMode, load and save that settings in loader)
-=======
-    return MaterialApp.router(
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      routerConfig: routes,
->>>>>>> parent of a9c3115 (latest codes)
     );
   }
 }
--- lib/screens/core/loader.dart
+++ lib/screens/core/loader.dart
@@ -16,61 +16,8 @@ class _LoaderScreenState extends State<LoaderScreen> {
     final firstLaunch = await storage.isFirstLaunch();
 
     if (firstLaunch) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      // varsayilan tema modunu oku sistemden
-      final darkMode;
-      if (ThemeMode.system == ThemeMode.light) {
-        darkMode = false;
-      } else {
-        darkMode = true;
-      }
-
-      // varsayilan sistemdeki dili okumak kodlari
-      final String defaultLocale =
-          Platform.localeName; // Returns locale string in the form 'en_US'
-      final langParts = defaultLocale.split("_");
-
-      // tr_TR
-
-      final supportedLanguages = ["en", "tr", "fr", "es"];
-      final lang;
-      if (supportedLanguages.contains(langParts[0].toLowerCase())) {
-        // cihazda olan dili destekliyoruz
-        lang = langParts[0].toLowerCase();
-      } else {
-        lang = "en";
-      }
-
-<<<<<<< HEAD
-      clientCubit.setLanguage(lang);
-
-      // await storage.setSettings(darkMode: darkMode, language: lang);
-=======
->>>>>>> parent of acd7694 (read system language and themeMode, load and save that settings in loader)
-=======
->>>>>>> parent of acd7694 (read system language and themeMode, load and save that settings in loader)
-=======
-      await storage.setSettings(darkMode: darkMode, language: lang);
->>>>>>> parent of a9c3115 (latest codes)
-      // tanitima gonder
-      // navigate to boarding screen
       GoRouter.of(context).replace("/boarding");
     } else {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      // ayarlari yukliyelim
-      final settings = await storage.getSettings();
-<<<<<<< HEAD
-
-      clientCubit.setLanguage(settings["language"]);
-      clientCubit.setThemeMode(settings["darkMode"]);
-=======
->>>>>>> parent of acd7694 (read system language and themeMode, load and save that settings in loader)
-=======
->>>>>>> parent of acd7694 (read system language and themeMode, load and save that settings in loader)
-=======
->>>>>>> parent of a9c3115 (latest codes)
       // ana ekrana git
       // navigate to home screen
       GoRouter.of(context).replace("/home");
